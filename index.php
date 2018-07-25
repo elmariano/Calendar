6651d8ba61a1623e964a7750deef73e0aa4edfaa
StayAwake14 <quartermaster226@gmail.com>
2018-07-25 10:32:13 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/styles.css">
+    </head>
+
+    <body>
+        
+    </body>
+</html>
